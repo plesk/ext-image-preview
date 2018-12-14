25f51d3ca3485175eb6af68bcb1cecd93cb20f9e
Pavel Kanin <pkanin@plesk.com>
2018-12-14 14:14:17 +0700

    BUGFIX PPP-36398 Handle click events before another handlers

--- htdocs/image-preview.js
+++ htdocs/image-preview.js
@@ -1,4 +1,5 @@
-// Copyright 1999-2016. Parallels IP Holdings GmbH.
+// Copyright 1999-2018. Plesk International GmbH. All rights reserved.
+
 Jsw.onReady(function () {
     var supportedExtensions = ['.gif', '.jpg', '.jpeg', '.png', '.bmp'];
     var rootUrl = '/modules/image-preview/external/';
@@ -6,33 +7,35 @@ Jsw.onReady(function () {
     LightboxOptions.fileLoadingImage = rootUrl + LightboxOptions.fileLoadingImage;
     LightboxOptions.fileBottomNavCloseImage = rootUrl + LightboxOptions.fileBottomNavCloseImage;
 
-    document.observe('click', function (event) {
+    var lightbox = new Lightbox();
+
+    document.addEventListener('click', function (e) {
         var target = event.findElement('a.fm-file-name-link');
         if (!target) {
             return;
         }
 
-        $$('a.fm-file-name-link').each(function (file) {
-            var name = file.down('span').innerHTML;
-
-            var isSupported = false;
-            supportedExtensions.each(function (extension) {
-                if (0 < name.lastIndexOf(extension)) {
-                    isSupported = true;
-                }
-            });
-            if (!isSupported) {
-                return;
+        var name = target.down('span').innerHTML;
+        var isSupported = false;
+        supportedExtensions.each(function (extension) {
+            if (0 < name.lastIndexOf(extension)) {
+                isSupported = true;
             }
-
-            var url = '/smb/file-manager/show-image/?file=' + encodeURIComponent(name),
-                newUrl = file.readAttribute('href').indexOf('/show?') > -1
-                    ? file.readAttribute('href').replace('/show?', '/show-image/?')
-                    : url;
-            file.writeAttribute('rel', 'lightbox[roadtrip]');
-            file.writeAttribute('href', newUrl);
         });
-    });
+        if (!isSupported) {
+            return;
+        }
 
-    new Lightbox();
+        e.preventDefault();
+        e.stopPropagation();
+
+        var url = '/smb/file-manager/show-image/?file=' + encodeURIComponent(name),
+            newUrl = target.readAttribute('href').indexOf('/show?') > -1
+                ? target.readAttribute('href').replace('/show?', '/show-image/?')
+                : url;
+        target.writeAttribute('rel', 'lightbox[roadtrip]');
+        target.writeAttribute('href', newUrl);
+        lightbox.start(target);
+        target.removeAttribute('rel');
+    }, true);
 });
